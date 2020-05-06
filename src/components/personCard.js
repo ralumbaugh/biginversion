58bc9f1869cc72e279db4acf3103c8b53c3374ab
Ryan Alumbaugh <ralumbaugh@gmail.com>
2020-05-06 13:35:35 -0700

    first commit

---
+++ src/components/personCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './personCard.css';
+
+const PersonCard = props => {
+    return(
+        <div className="card">
+            <h1> {props.lastName}, {props.firstName}</h1>
+            <p>Age: {props.age}</p>
+            <p>Hair Color: {props.hairColor}</p>
+        </div>
+    );
+}
+
+export default PersonCard
